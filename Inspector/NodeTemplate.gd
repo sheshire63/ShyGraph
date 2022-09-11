aac42fe1e53fed0c98fe09a32aa4c81ab419caf6
sheshire63 <sheshire63@gmail.com>
2022-09-11 13:06:06 +0200

    added virtual functions to node template

--- Inspector/NodeTemplate.gd
+++ Inspector/NodeTemplate.gd
@@ -1,3 +1,19 @@
 tool
 extends ShyGraphNode
 
+
+
+func _save_data() -> Dictionary:
+	return {}
+
+
+func _load_data(data:= {}) -> void:
+	pass
+
+
+func _delete() -> void:
+	pass
+
+
+func _copy(copy) -> void:#if you need to set somthing in the copy.
+	pass
